8f0d4315c551d352e1187b20b00324a1293e4d1c
ZeCroque <zobilezeub@gmail.com>
2018-04-25 01:36:31 +0200

    POO

--- L2/S2/POO/Projet/main.cpp
+++ L2/S2/POO/Projet/main.cpp
@@ -75,10 +75,11 @@ int main()
 
     //DEBUG//
     table.push_back(Carte(11,1));
-    table.push_back(Carte(13,1));
     table.push_back(Carte(12,1));
-    table.push_back(Carte(1,1));
+    table.push_back(Carte(13,1));
+    table.push_back(Carte(1,2));
     table.push_back(Carte(10,1));
+    table.push_back(Carte(9,1));
     verifierCombinaison(joueurs, table);
     //afficherTable(table);
 
@@ -94,10 +95,141 @@ int main()
   return 0;
 }
 
+bool verifierQuinteFlush(Vecteur<Carte> cartes)
+{
+  int supCount;
+  Carte hauteurmax; //Hauteur max trouvée lors de la recherche de suite
+
+  //Parcours de toute les cartes
+  for(int i=0; i<cartes.size(); i++)
+  {
+    //Variables
+    Carte tmp=cartes[i];  //Tampon pour stocker la carte que l'on compare aux autres
+    Carte hauteur;  //Tampon qui stocke la hauteur de suite la plus grande trouvée jusqu'ici
+    bool supTrouve=false; //Booléen indiquant si l'on a trouvé une hauteur plus grande que le tampon
+    supCount=0; //Compteur du nombre de fois qu'une hauteur plus grande que le tampon est trouvé
+
+    //Boucle qui recommencera tant que l'on trouvera une carte plus grande que celle examinée
+    do
+    {
+      int j=0;
+      supTrouve=false;
+
+      //Parcoure les cartes à partir de 0 jusqu'à arriver à la fin du tableau OU jusqu'à trouver une carte qui répondes aux critères suivants :
+      //1 - La hauteur de la carte est supérieure à celle du tampon ET le tampon n'est pas un roi (13)
+      //        OU
+      //    Le tampon est un roi (13) ET la carte est un as (1)
+      //2 - La couleur de la carte est la même que celle du tampon
+      while(j!=cartes.size() && ((tmp.getValeur()==13 && cartes[j].getValeur()!=1) || (tmp.getValeur()+1!=cartes[j].getValeur() && tmp.getValeur()!=13) || tmp.getFamille()!=cartes[j].getFamille()))
+      {
+        j++;
+      }
+
+      //Si le while a trouvé une carte correspondant aux critères précédents (fin du tableau non atteite)
+      if(j!=cartes.size())
+      {
+        //On incrémente le compteur, met dans le tampon la carte trouvée, et l'on indique qu'il faut recommencer
+        supCount++;
+        tmp=cartes[j];
+        supTrouve=true;
+      }
+    } while(supTrouve);
+
+    //Si l'on vient de déterminer que 4 cartes ou plus suivent la première
+    if(supCount>3)
+    {
+      //L'on fixe le tampon de hauteur à la carte la plus haute que l'on a examinée
+      hauteur=tmp;
+      //Si ledit tampon est plus grand que la hauteur max précédemment enregistrée
+      if(hauteur.getValeur()>hauteurmax.getValeur() || hauteur.getValeur()==1)
+      {
+        //La hauteur max devient le tampon
+        hauteurmax=hauteur;
+      }
+    }
+  }
+
+  //Si l'on a trouvé une quinte (la hauteur max est différente de celle par défaut)
+  if(hauteurmax.getValeur()!=0)
+  {
+    //On affiche et retourne un résultat positif
+    cout<<"Quinte flush hauteur "<<hauteurmax<<endl;
+    return true;
+  }
+  //Sinon on retourne un résultat négatif
+  return false;
+}
+
+bool verifierQuinte(Vecteur<Carte> cartes)
+{
+  int supCount;
+  Carte hauteurmax; //Hauteur max trouvée lors de la recherche de suite
+
+  //Parcours de toute les cartes
+  for(int i=0; i<cartes.size(); i++)
+  {
+    //Variables
+    Carte tmp=cartes[i];  //Tampon pour stocker la carte que l'on compare aux autres
+    Carte hauteur;  //Tampon qui stocke la hauteur de suite la plus grande trouvée jusqu'ici
+    bool supTrouve=false; //Booléen indiquant si l'on a trouvé une hauteur plus grande que le tampon
+    supCount=0; //Compteur du nombre de fois qu'une hauteur plus grande que le tampon est trouvé
+
+    //Boucle qui recommencera tant que l'on trouvera une carte plus grande que celle examinée
+    do
+    {
+      int j=0;
+      supTrouve=false;
+
+      //Parcoure les cartes à partir de 0 jusqu'à arriver à la fin du tableau OU jusqu'à trouver une carte qui répondes aux critères suivants :
+      //    La hauteur de la carte est supérieure à celle du tampon ET le tampon n'est pas un roi (13)
+      //        OU
+      //    Le tampon est un roi (13) ET la carte est un as (1)
+      while(j!=cartes.size() && ((tmp.getValeur()==13 && cartes[j].getValeur()!=1) || (tmp.getValeur()+1!=cartes[j].getValeur() && tmp.getValeur()!=13)))
+      {
+        j++;
+      }
+
+      //Si le while a trouvé une carte correspondant aux critères précédents (fin du tableau non atteite)
+      if(j!=cartes.size())
+      {
+        //On incrémente le compteur, met dans le tampon la carte trouvée, et l'on indique qu'il faut recommencer
+        supCount++;
+        tmp=cartes[j];
+        supTrouve=true;
+      }
+    } while(supTrouve);
+
+    //Si l'on vient de déterminer que 4 cartes ou plus suivent la première
+    if(supCount>3)
+    {
+      //L'on fixe le tampon de hauteur à la carte la plus haute que l'on a examinée
+      hauteur=tmp;
+      //Si ledit tampon est plus grand que la hauteur max précédemment enregistrée
+      if(hauteur.getValeur()>hauteurmax.getValeur() || hauteur.getValeur()==1)
+      {
+        //La hauteur max devient le tampon
+        hauteurmax=hauteur;
+      }
+    }
+  }
+
+  //Si l'on a trouvé une quinte (la hauteur max est différente de celle par défaut)
+  if(hauteurmax.getValeur()!=0)
+  {
+    //On affiche et retourne un résultat positif
+    cout<<"Quinte hauteur "<<hauteurmax<<endl;
+    return true;
+  }
+  //Sinon on retourne un résultat négatif
+  return false;
+}
+
+
+
 void verifierCombinaison(const Vecteur<Joueur>& joueurs, const Vecteur<Carte>& table)
 {
   //Compteurs
-  int i, j, k, l, m;
+  int i, j, k;
 
   for (i=0; i<joueurs.size(); i++)
   {
@@ -106,10 +238,12 @@ void verifierCombinaison(const Vecteur<Joueur>& joueurs, const Vecteur<Carte>& t
     cartes.resize(joueurs[i].getMain().size()+table.size());
 
     //Remplissage du tableau nouvellement créé
+    //D'abord avec la main du joueur
     for (j=0; j<joueurs[i].getMain().size(); j++)
     {
       cartes[j]=joueurs[i].getMain()[j];
     }
+    //Puis avec le board
     for(k=0; k<table.size(); k++, j++)
     {
       cartes[j]=table[k];
@@ -127,84 +261,14 @@ void verifierCombinaison(const Vecteur<Joueur>& joueurs, const Vecteur<Carte>& t
 
 
     //Affichage des meilleures cartes
-
-    //Test quinte
-    bool quinteTrouvee=false;
-    k=0;
-    do
+    bool combinaisonTrouvee=false;
+    combinaisonTrouvee=verifierQuinteFlush(cartes);
+    if(!combinaisonTrouvee)
     {
-      j=0;
-      m=0;
-      Carte tmp=cartes[k];
-      for(l=0; l<cartes.size(); l++)
-      {
-        if((tmp.getValeur()==13 && cartes[l].getValeur()==1) || tmp.getValeur()+1==cartes[l].getValeur())
-        {
-          if(tmp.getFamille()==cartes[l].getFamille())
-          {
-            m++;
-          }
-          j++;
-          tmp=cartes[l];
-        }
-      }
-      for(l=cartes.size()-1; l>0; l--)
-      {
-        if((tmp.getValeur()==13 && cartes[l].getValeur()==1) || tmp.getValeur()+1==cartes[l].getValeur())
-        {
-          if(tmp.getFamille()==cartes[l].getFamille())
-          {
-            m++;
-          }
-          j++;
-          tmp=cartes[l];
-        }
-      }
-      Carte hauteur=tmp;
-      if(j<4)
-      {
-        Carte tmp=cartes[k];
-        for(l=0; l<cartes.size(); l++)
-        {
-          if(tmp.getValeur()!=1 && tmp.getValeur()-1==cartes[l].getValeur())
-          {
-            if(tmp.getFamille()==cartes[l].getFamille())
-            {
-              m++;
-            }
-            j++;
-            tmp=cartes[l];
-          }
-        }
-        for(l=cartes.size()-1; l>0; l--)
-        {
-          if(tmp.getValeur()!=1 && tmp.getValeur()-1==cartes[l].getValeur())
-          {
-            if(tmp.getFamille()==cartes[l].getFamille())
-            {
-              m++;
-            }
-            j++;
-            tmp=cartes[l];
-          }
-        }
-      }
-      if(j>3)
-      {
-        if(m<4)
-        {
-          cout<<"Quinte hauteur "<<hauteur<<endl;
-        }
-        else
-        {
-          cout<<"Quinte flush hauteur "<<hauteur<<endl;
-        }
-        quinteTrouvee=true;
-      }
-
-      k++;
-    } while(quinteTrouvee==false && k!=cartes.size());
-
+      combinaisonTrouvee=verifierQuinte(cartes);
+    }
+    //TODO
+    //Autre vérifs
   }
 }
 
