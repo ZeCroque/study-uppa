a5f825d5b1665c301aae9363ad2a08cebe9fc8c8
ZeCroque <zobilezeub@gmail.com>
2018-12-05 01:39:44 +0100

    graphe

--- L3/S1/Graphes/TP1/Graphe.h
+++ L3/S1/Graphes/TP1/Graphe.h
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <cstdlib>
 
 class Graphe
 {
   private:
     int _nbSommet;
-    std::vector<std::vector<bool>> _matrixLiens;
+    std::vector<std::vector<bool> > _matrixLiens;
   public:
     Graphe();
     Graphe(int nbSommet);
     void ajouterLien(int s1, int s2);
     void afficherGraphe();
+    void trouverComposanteFortementConnexe();
+    void parcoursHistorique();
     friend std::ostream& operator << (std::ostream& os, const Graphe& g);
 };
